555033808e3f1842cc47db9f4aaae22f276b4e97
nanaa004 <fernandaffranca004@gmail.com>
2025-04-23 11:31:22 -0300

    foi mais uma vez

---
+++ testeOU/Program.cs
@@ -0,0 +1,13 @@
+﻿string cargo;
+
+Console.WriteLine("Informe qual seu cargo na universidade: ");
+cargo = Console.ReadLine();
+
+if (cargo == "aluno" || cargo == "professor")
+        {
+    Console.WriteLine("Você pode entrar:D");
+}
+else
+{
+    Console.WriteLine("Não pode entrar:(");
+}

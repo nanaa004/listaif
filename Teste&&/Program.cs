42795e7c79a03fbd9c8acc11d4c7ae80fbc106e3
nanaa004 <fernandaffranca004@gmail.com>
2025-04-23 10:27:00 -0300

    foi de novo

--- Teste&&/Program.cs
+++ Teste&&/Program.cs
@@ -4,15 +4,17 @@ int idade = int.Parse(Console.ReadLine());
 Console.WriteLine("Digite a altura em centimetros");
 int altura = int.Parse(Console.ReadLine());
 
-if (idade >= 18) && (altura > 1, 65);
+if (idade >= 18 && altura >= 165)
    {
-    Console.WriteLine("Você pode se alistar!");
+    Console.WriteLine("Você pode se alistar:D");
 
 }
-else;
-    [
-        Console.WriteLine("Você não pode se alistar :(");
-        ]
+else
+{
+Console.WriteLine("Você não pode se alistar :(");
+}
+        
+        
 
 
 
